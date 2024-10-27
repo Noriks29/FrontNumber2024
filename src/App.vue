773037dd90250337bbff917e0c0c4441ab4d20e6
Noriks29 <sehinr292@gmail.com>
2024-10-27 12:49:34 +0300

    LoadDisplay

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,12 @@
 <template>
+  <div id="loadProcess" style="display: none;">
+    <div>
+      <svg class="spinner" width="65px" height="65px" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">
+        <circle class="path" fill="none" stroke-width="6" stroke-linecap="round" cx="33" cy="33" r="30"></circle>
+      </svg>
+
+    </div>
+  </div>
   <WelcomeScreen v-if="StartWort==0" @WorkStatus="StartWortEvent"/>
   <LoginPage v-if="!statusLogin && StartWort==1" @LoginStatus="GetStartetMain"/>
   <MainPage v-if="statusLogin && StartWort==1" :loginData="loginData" @LoginStatus="GetStartetMain" />
@@ -61,5 +69,59 @@ async mounted() {
 </script>
 
 <style lang="scss">
-  
+
+
+
+#loadProcess{
+  width: 100vw;
+    height: 100vh;
+    position: absolute;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    z-index: 10;
+    background-color: #ffffffa8;
+}
+  // Here is where the magic happens
+
+$offset: 187;
+$duration: 1.4s;
+
+.spinner {
+  animation: rotator $duration linear infinite;
+}
+
+@keyframes rotator {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(270deg); }
+}
+
+.path {
+  stroke-dasharray: $offset;
+  stroke-dashoffset: 0;
+  transform-origin: center;
+  animation:
+    dash $duration ease-in-out infinite, 
+    colors ($duration*4) ease-in-out infinite;
+}
+
+@keyframes colors {
+	0% { stroke: #4285F4; }
+	25% { stroke: #DE3E35; }
+	50% { stroke: #F7C223; }
+	75% { stroke: #1B9A59; }
+  100% { stroke: #4285F4; }
+}
+
+@keyframes dash {
+ 0% { stroke-dashoffset: $offset; }
+ 50% {
+   stroke-dashoffset: calc($offset/4);
+   transform:rotate(135deg);
+ }
+ 100% {
+   stroke-dashoffset: $offset;
+   transform:rotate(450deg);
+ }
+}
 </style>
--- src/Js/RestFetchService.js
+++ src/Js/RestFetchService.js
@@ -1,8 +1,8 @@
 import { adress } from "./ServerConfig";
 import DebugData from "../assets/resource/DebugDataFetch.json"
 const ShowFetchData = true
-const DEBUGMODE = false
-/*
+const DEBUGMODE = true
+
 function DisplayLoad(status){
     const element = document.getElementById("loadProcess");
     if(status){
@@ -11,9 +11,14 @@ function DisplayLoad(status){
     else{
         element.style.display = "none"
     }
-}*/
+}
 async function FetchGet(http){
-    if (DEBUGMODE) return DebugData[http]
+    DisplayLoad(true)
+    if (DEBUGMODE) {
+        DisplayLoad(false)
+        return DebugData[http]
+        
+    }
     try {
         const response = await fetch(adress+http);
         if (!response.ok) {
@@ -23,19 +28,26 @@ async function FetchGet(http){
         }
         else{
             let rezult = await response.json()
+            DisplayLoad(false)
             if(ShowFetchData) console.log(http, rezult)
             return rezult;
         }
     } catch (error) {
         console.log('Error during fetch:', error);
         alert("Ошибка запроса, дальнейшая работа может быть некорректной!" + error)
+        DisplayLoad(false)
         return undefined
     }
 
 }
 
 async function FetchPost(http,datapost){
-    if (DEBUGMODE) return DebugData[http]
+    DisplayLoad(true)
+    if (DEBUGMODE){
+        DisplayLoad(false)
+        return DebugData[http]
+       
+    } 
     if(ShowFetchData) console.log(JSON.stringify(datapost))
     try {
         const response = await fetch(adress+http,{
@@ -53,11 +65,13 @@ async function FetchPost(http,datapost){
         else{
             let rezult = await response.json()
             if(ShowFetchData) console.log(http, rezult)
+                DisplayLoad(false)
             return rezult;
         }
         } catch (error) {
             console.log('Error save:', error);
             alert("ОШИБКА ОТПРАВКИ  " + error)
+            DisplayLoad(false)
             return undefined;
         }
 }
